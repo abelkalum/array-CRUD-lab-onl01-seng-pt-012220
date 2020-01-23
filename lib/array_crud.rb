cf57d4c3b28b70f34d48d675a158e4da9d682240
Abel Kalum <chumbabel@gmail.com>
2020-01-23 21:22:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -36,7 +36,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  
+  friends = ["Koech", "Siria", "Caleb", "Stanley"]
+  friends[0]
 end
 
 def retrieve_last_element_from_array(array)

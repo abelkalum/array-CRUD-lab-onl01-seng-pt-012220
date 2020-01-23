cfae9886f43c2db32df02b5133e9d73d221d9ddb
Abel Kalum <chumbabel@gmail.com>
2020-01-23 21:28:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  friends = ["Koech", "Siria", "Caleb", "Stanley"]
+  array = ["wow", "I", "am", "really", "learning"]
   friends << "Willy"
   puts friends.inspect
 end
